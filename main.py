b699ce56afc97de5849e5331cd8611b0b2d6f66a
Raisul Sohan <btvdhk@gmail.com>
2020-01-30 14:29:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+
+print("Welcome to the Triangle Solver app")
+
+a = float(input("\nwhat is the first leg of the triangle: "))
+b = float(input("what is the second leg of the triangle: "))
+
+c = math.sqrt(a**2 + b**2)
+c = round(c, 3)
+
+area = 0.5*a*b
+area = round(area, 3)
+
+print(f"\nFor a triangle with legs of {a} and {b} the hypotenus is {c}.")
+print(f"For a triangle with legs of {a} and {b} the area is {area}.")
